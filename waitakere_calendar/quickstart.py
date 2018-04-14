7e1119f9a6d264351a88ee6198abc0139afbe848
graham-desktop <graham_oliver@yahoo.com>
2018-04-14 22:08:54 +1200

    progress

--- waitakere_calendar/quickstart.py
+++ waitakere_calendar/quickstart.py
@@ -3,8 +3,11 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import datetime
+import pprint
 
-# Setup the Calendar API
+PERIOD_TEXT = "Period"
+
+# Setup the Calendar API (Boiler Plate)
 SCOPES = 'https://www.googleapis.com/auth/calendar' # read / write
 store = file.Storage('credentials.json')
 creds = store.get()
@@ -13,17 +16,18 @@ if not creds or creds.invalid:
     creds = tools.run_flow(flow, store)
 service = build('calendar', 'v3', http=creds.authorize(Http()))
 
-# Call the Calendar API
-now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-print('Getting the upcoming 10 events')
-events_result = service.events().list(calendarId='primary', timeMin=now,
-                                      maxResults=10, singleEvents=True,
-                                      orderBy='startTime').execute()
-events = events_result.get('items', [])
+# Get all events in the primary calendar
+all_events = [] # a list, built up in lots of 250. Each list item is a dict
+page_token = None
+while True:
+    events_lot = service.events().list(calendarId='primary', pageToken=page_token).execute() # a dict
+    for event in events_lot["items"]:
+        all_events.append(event) 
 
-if not events:
-    print('No upcoming events found.')
-for event in events:
-    start = event['start'].get('dateTime', event['start'].get('date'))
-    print(event['start']['dateTime'], event['summary'])
+    page_token = events_lot.get('nextPageToken')
+    if page_token is None:
+        break
 
+for event in all_events: #list of dictionaries
+    if PERIOD_TEXT in event['summary']:
+        print(event['start']['dateTime'], event['summary'])
